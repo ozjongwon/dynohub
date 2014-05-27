d51503a932827b9099786aa4a01a17f4b24f0e1c
Jong-won Choi <oz.jongwon.choi@gmail.com>
2014-05-27 17:30:27 +1000

    Adds aliases to enable [with/without]-binary-reader-writer in dynolite ns

--- src/dynohub/dynolite.clj
+++ src/dynohub/dynolite.clj
@@ -12,6 +12,32 @@
   (:require [ozjongwon.dynohub  :as dh]))
 
 ;;
+;; Copy paste from:
+;; https://github.com/sritchie/jackknife/blob/master/src/jackknife/def.clj#L4
+(defmacro defalias
+  "Defines an alias for a var: a new var with the same root binding (if
+  any) and similar metadata. The metadata of the alias is its initial
+  metadata (as provided by def) merged into the metadata of the original."
+  ([name orig]
+     `(do
+        (alter-meta!
+         (if (.hasRoot (var ~orig))
+           (def ~name (.getRawRoot (var ~orig)))
+           (def ~name))
+         ;; When copying metadata, disregard {:macro false}.
+         ;; Workaround for http://www.assembla.com/spaces/clojure/tickets/273
+         #(conj (dissoc % :macro)
+                (apply dissoc (meta (var ~orig)) (remove #{:macro} (keys %)))))
+        (var ~name)))
+  ([name orig doc]
+     (list `defalias (with-meta name (assoc (meta name) :doc doc)) orig)))
+
+;; aliases
+
+(defalias with-binary-reader-writer dh/with-binary-reader-writer)
+(defalias without-binary-reader-writer dh/without-binary-reader-writer)
+
+;;
 ;; DynamoDB connection
 ;;
 (defonce default-client-opts (atom {:access-key "accesskey" :secret-key "secretkey"
