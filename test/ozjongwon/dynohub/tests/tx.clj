35182b070a9e166d96deb24ec477d2a7f1195e63
Jong-won Choi <oz.jongwon.choi@gmail.com>
2014-06-03 15:52:13 +1000

    Some usage idea

---
+++ test/ozjongwon/dynohub/tests/tx.clj
@@ -0,0 +1,47 @@
+;;;;   -*- Mode: clojure; encoding: utf-8; -*-
+;;
+;; Copyright (C) 2014 Jong-won Choi
+;; All rights reserved.
+;;
+;;;; Commentary:
+;;
+;;
+;;
+;;;; Code:
+
+(with-transaction [t1]
+  (tx/put-item example-table-name {:item-id "Item1"} :tx t1)
+  (tx/put-item example-table-name {:item-id "Item2"} :tx t1)
+  (commit t1)
+  (delete t1))
+=>
+(binding [*current-tx* (new-transaction)]
+  (tx/put-item example-table-name {:item-id "Item1"})
+  (tx/put-item example-table-name {:item-id "Item2"})
+  (commit *current-tx*)
+  (delete *current-tx*))
+
+(with-transaction [t1]
+  (tx/put-item example-table-name {:item-id "conflictingTransactions_Item1" "WhichTransaction?" "t1"} :tx t1)
+  (tx/put-item example-table-name {:item-id "conflictingTransactions_Item2"} :tx t1)
+  (with-transaction [t2]
+    (tx/put-item example-table-name {:item-id "conflictingTransactions_Item1" "WhichTransaction?" "t2 - I win!"}
+                 :tx t2)
+    (try (commit *current-tx*)
+         (catch TransactionRolledBackException _
+           (delete t1)))
+    (tx/put-item example-table-name {:item-id "conflictingTransactions_Item3"} :tx t2)
+    (commit t2)
+    (delete t2)
+
+    (tx/get-item example-table-name {:item-id "conflictingTransactions_Item1"} :isolation-level :uncommited)
+
+
+
+
+
+
+
+
+
+;;; TX.CLJ ends here
