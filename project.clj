331d90d081da026109262a1db34064a8637b5597
Jongwon Choi <ozjongwon.choi@gmail.com>
2014-07-04 16:18:18 +1000

    Bumps to 1.1.0 RC4

--- project.clj
+++ project.clj
@@ -9,7 +9,7 @@
 ;;
 ;;
 ;;;; Code:
-(defproject com.ozjongwon/dynohub "1.1.0-RC3"
+(defproject com.ozjongwon/dynohub "1.1.0-RC4"
   :author "Jong-won Choi"
   :description "Clojure DynamoDB client with or without Transaction layer"
   :url "https://github.com/ozjongwon/dynohub"
